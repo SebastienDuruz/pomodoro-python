71aa8daa8d580c7755bd14f954f9f4cfdbfd2337
Sébastien Duruz <sebastienduruz@protonmail.com>
2022-01-11 09:42:43 +0100

    Initial Commit

---
+++ Pages/main_page.py
@@ -0,0 +1,18 @@
+# ETML
+# Author : Sébastien Duruz
+# Date : 10.01.2021
+
+
+import tkinter as tk
+
+
+class MainPage:
+
+    def __init__(self):
+        window = tk.Tk()
+        title_home = tk.Label(window, text="Main Page title")
+        title_home.pack()
+        window.mainloop()
+
+
+
---
+++ main.py
@@ -0,0 +1,19 @@
+# ETML
+# Author : Sébastien Duruz
+# Date : 10.01.2021
+
+
+import Pages.main_page as main_page
+
+
+def main():
+    """
+    Main entry of the program
+    """
+
+    main_page.MainPage()
+
+
+# Execute main function if project called
+if __name__ == '__main__':
+    main()
